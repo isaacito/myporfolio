8d35a8ce7fcf83c1064e61dc8b6a0f4852958a86
Isaac Weinbach <isaacito@aol.com>
2014-11-10 17:02:36 -0500

    new htmlrevamp

--- server123.rb
+++ server123.rb
@@ -1,9 +1,7 @@
 require 'sinatra'
 
 get '/' do 
-
-File.read(File.join('public', 'max_for_ruby_server.rb'))
-
+File.read(File.join('public', 'about_me.html'))
 
 end
 
@@ -11,7 +9,7 @@ get '/iwein.txt'do
 
 end
 
-get '/about' do
-	File.read(File.join('public', 'about_me.html'))
-
+get '/homework' do
+	
+File.read(File.join('public', 'max_for_ruby_server.rb'))
 end

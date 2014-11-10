e425d2dae672d25f729681cf6dcffb388ad66b1b
Isaac Weinbach <isaacito@aol.com>
2014-11-10 09:19:43 -0500

    new html

--- server123.rb
+++ server123.rb
@@ -4,8 +4,14 @@ get '/' do
 
 File.read(File.join('public', 'max_for_ruby_server.rb'))
 
+
 end
 
 get '/iwein.txt'do
+
+end
+
+get '/about' do
+	File.read(File.join('public', 'about_me.html'))
 
 end

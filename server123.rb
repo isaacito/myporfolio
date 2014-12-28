596bef2f95b9995fed8cf751e6ede1d83a93c444
Isaac Weinbach <isaacito@aol.com>
2014-12-27 20:09:00 -0500

    updated contact me and overall style

--- server123.rb
+++ server123.rb
@@ -4,12 +4,3 @@ get '/' do
 File.read(File.join('public', 'about_me.html'))
 
 end
-
-get '/iwein.txt'do
-
-end
-
-get '/homework' do
-	
-File.read(File.join('public', 'max_for_ruby_server.rb'))
-end
